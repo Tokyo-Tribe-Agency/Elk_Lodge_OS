2c5c53f99ae81b840fbe80046bb922f956ca9ecf
Helana <helanan@gmail.com>
2017-10-29 17:01:14 -0500

    added new models, added to the views.py file the index url, added to urls the index route

--- lodge/admin.py
+++ lodge/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from . import models
+
+admin.site.register(models.Elks)
+admin.site.register(models.Photos)
+admin.site.register(models.Payments)
+admin.site.register(models.Donations)
+admin.site.register(models.Events)
+admin.site.register(models.Newsletter)
+
 # Register your models here.
