b292748639e3fd9d2bda4a3e036d0882be047b5b
leead4 <leead4@gmail.com>
2017-12-29 00:16:28 -0600

    almost mvp need to adapt forms.py

--- lodge/views/views.py
+++ lodge/views/views.py
@@ -7,7 +7,27 @@ from datetime import date
 from django import forms
 from django.db.models import Q
 from lodge.models.models import *
+from django.conf import settings
+import braintree
 
+
+
+braintree.Configuration.configure(
+    braintree.Environment.Sandbox,
+    'dwnqbjvjdjc8ysjp',
+    'kgzvv6qhfzq6dq5y',
+    '4e0b90ddfad27f835c658a2f76850504'
+)
+
+TRANSACTION_SUCCESS_STATUSES = [
+    braintree.Transaction.Status.Authorized,
+    braintree.Transaction.Status.Authorizing,
+    braintree.Transaction.Status.Settled,
+    braintree.Transaction.Status.SettlementConfirmed,
+    braintree.Transaction.Status.SettlementPending,
+    braintree.Transaction.Status.Settling,
+    braintree.Transaction.Status.SubmittedForSettlement
+]
 # Create your views here.
 
 def index(request):
@@ -33,6 +53,49 @@ def register(request):
 	template_name = 'register.html'
 	print("request", request.path)
 	return render(request, template_name, {})
+
+def new_checkout(request):
+    client_token = braintree.ClientToken.generate()
+    template_name = 'paypal/new.html'
+    return render(request, template_name, {'client_token': client_token})
+
+def show_checkout(request, transaction_id):
+    transaction = braintree.Transaction.find(transaction_id)
+    template_name = "paypal/show.html"
+    result = {}
+    if transaction.status in TRANSACTION_SUCCESS_STATUSES:
+        result = {
+            'header': 'Sweet Success!',
+            'icon': 'success',
+            'message': 'Your test transaction has been successfully processed. See the Braintree API response and try again.'
+        }
+    else:
+        result = {
+            'header': 'Transaction Failed',
+            'icon': 'fail',
+            'message': 'Your test transaction has a status of ' + transaction.status + '. See the Braintree API response and try again.'
+        }
+
+    return render(request, template_name, {'transaction': transaction, 'result': result})
+
+def create_checkout(request):
+	form = request.POST
+	data = 
+	
+	result = braintree.Transaction.sale({
+	'amount': request.form.cleaned_data['amount'],
+	'payment_method_nonce': request.form.cleaned_data['payment_method_nonce'],
+	'options': {
+	"submit_for_settlement": True
+	}
+	})
+	if result.is_success or result.transaction:
+		return redirect('show_checkout', transaction_id=result.transaction.id)
+	else:
+		for x in result.errors.deep_errors: flash('Error: %s: %s' % (x.code, x.message))
+		return redirect('new_checkout')
+
+
 
 # def about(request):
 # 	template_name = 'about.html'
